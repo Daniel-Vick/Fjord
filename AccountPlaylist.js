4bd8f5a44f776052ffce826a4f4189d6ac789033
Daniel Vick <dvick@ucsd.edu>
2017-05-26 00:07:12 -0700

    Persistent votes 2

--- AccountPlaylist.js
+++ AccountPlaylist.js
@@ -14,11 +14,9 @@ class AccountPlaylist extends Component {
   _add(input1, input2, input3, input4) {
     this.itemsRef.push({ username: input1, id: input2, name: input3, score: input4})
   }
-  test() {
+  addPlaylist() {
     var data = { playlistName: this.props.name, playlistId: this.props.id, userId: this.props.user };
-    console.log("#####################TEST##############");
-    console.log(this.props.name + " " + this.props.id + " " + this.props.user);
-    return fetch(ip+':8888/Insert', {method: 'PUT', headers: {'Accept' : 'application/json', 'Content-Type': 'application/json'}, body: JSON.stringify(data)})
+    return fetch(ip+':8888/AddPlaylist', {method: 'PUT', headers: {'Accept' : 'application/json', 'Content-Type': 'application/json'}, body: JSON.stringify(data)})
     .then((response) => console.log(response))
     
     .catch((error) => {
@@ -27,7 +25,7 @@ class AccountPlaylist extends Component {
   }
   render() {
     return(
-           <TouchableHighlight onPress={() => this.test()} style={{flex:1, borderBottomWidth:0.5, marginLeft:10, marginRight:10, borderColor:"white"}}>
+           <TouchableHighlight onPress={() => this.addPlaylist()} style={{flex:1, borderBottomWidth:0.5, marginLeft:10, marginRight:10, borderColor:"white"}}>
            <View style={{flex:1}}>
            <Text style={{flex:2, color:"white", fontWeight:'bold', fontSize:15, marginTop:5}}>{this.props.name}</Text>
            <Text style={{flex:1, color:"#BBBBBB", fontSize:12, marginBottom:5}}>{this.props.user}</Text>
